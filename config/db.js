c542d9594f3e70e0c23e92a92a06f198cf584424
m2000 <baranbarankaya1@gmail.com>
2026-03-04 19:49:09 +0300

    OFO Hotel DB ve Premium Tasarım Güncellemesi

--- config/db.js
+++ config/db.js
@@ -2,12 +2,13 @@ const mysql = require('mysql2');
 
 const pool = mysql.createPool({
     host: 'localhost',
-    user: 'u976935047_otelsitesi',         // Senin MySQL Kullanıcın
-    password: '1kpm@+g:H',           // Şifren (Değişmediyse)
-    database: 'u976935047_otel', // Senin Veritabanı İsmin
+    // DİKKAT: Burası 'u976935047_otel' olmalı (Hata mesajında 'otelsitesi' yazmışsın)
+    user: 'u976935047_otel',         
+    password: '1kpm@+g:H',           
+    database: 'u976935047_otelsitesi', 
     waitForConnections: true,
     connectionLimit: 10,
     queueLimit: 0
 });
 
-module.exports = pool.promise();    
+module.exports = pool.promise();
