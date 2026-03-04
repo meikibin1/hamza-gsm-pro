1d9814d44d5a5a527c221e1eb8b4476686423dfb
m2000 <baranbarankaya1@gmail.com>
2026-03-04 19:52:54 +0300

    DB baglanti hatasi duzeltildi ve tasarim guncellendi

--- config/db.js
+++ config/db.js
@@ -5,7 +5,7 @@ const pool = mysql.createPool({
     // DİKKAT: Burası 'u976935047_otel' olmalı (Hata mesajında 'otelsitesi' yazmışsın)
     user: 'u976935047_otel',         
     password: '1kpm@+g:H',           
-    database: 'u976935047_otelsitesi', 
+    database: 'u976935047_otel', 
     waitForConnections: true,
     connectionLimit: 10,
     queueLimit: 0
